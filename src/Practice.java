7578a9338f3f1c3472faa30b0d3f5cc1acf7c953
akurfurst <19035144+akurfurst@users.noreply.github.com>
2026-09-23 20:23:04 -0700

    Finished last problems and passed all tests

--- src/Practice.java
+++ src/Practice.java
@@ -116,7 +116,12 @@ public class Practice {
      * @return the number of levels in the tree
      */
     public static int levelCount(BinaryTreeNode<?> root) {
-        return 0;
+        if(root == null) return 0;
+
+        int left = levelCount(root.left);
+        int right = levelCount(root.right);
+
+        return Math.max(left, right) + 1;
     }
 
 
@@ -144,7 +149,9 @@ public class Practice {
      * @return the sum of the nodes at the given level
      */
     public static int sumAtLevel(BinaryTreeNode<Integer> root, int level) {
-        return 0;
+        if(root == null) return 0;
+        if(level == 1) return root.data;
+        return sumAtLevel(root.left, level-1) + sumAtLevel(root.right, level-1);
     }
 
 
@@ -159,6 +166,18 @@ public class Practice {
      * @return true if the sums are equal, false otherwise
      */
     public static boolean sumMatch(BinaryTreeNode<Integer> root, ListNode<Integer> head) {
-        return false;
+        ListNode<Integer> current = head;
+        int listTotal = 0;
+
+        while(current != null){
+            listTotal += current.data;
+            current = current.next;
+        }
+        return treeSum(root) == listTotal;
+    }
+
+    private static int treeSum(BinaryTreeNode<Integer> root){
+        if(root == null) return 0;
+        return root.data + treeSum(root.left) + treeSum(root.right);
     }
 }
